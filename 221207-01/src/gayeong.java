24ab15c44719047fb1715a8ef60eae0e90f93734
GGG <GGG@DESKTOP-8N3GG4O>
2022-12-07 16:31:39 +0900

    내이름이야

--- 221207-01/src/gayeong.java
+++ 221207-01/src/gayeong.java
@@ -3,6 +3,7 @@ public class gayeong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("정혜인");
 		System.out.println("가영조장");
 		System.out.println("가영안녕");
 
