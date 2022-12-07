f3d86fb6298c9eecea1ed66e0cdb9188b4012f36
GGG <GGG@DESKTOP-8N3GG4O>
2022-12-07 16:04:27 +0900

    가영안녕

--- 221207-01/src/gayeong.java
+++ 221207-01/src/gayeong.java
@@ -3,7 +3,8 @@ public class gayeong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("가영조장");
+		System.out.println("가영조장");
+		System.out.println("가영안녕");
 	}
 
 }
