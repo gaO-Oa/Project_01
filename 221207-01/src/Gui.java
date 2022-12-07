7b4209627c942f50e7dc8cd49ef9f36fc5c68a47
GGG <GGG@DESKTOP-8N3GG4O>
2022-12-07 16:50:31 +0900

    버튼을 하나더 만들어보았어요...

--- 221207-01/src/Gui.java
+++ 221207-01/src/Gui.java
@@ -7,11 +7,14 @@ import javax.swing.JPanel;
 class MyFrame extends JFrame {
 	public MyFrame() {
 		JPanel pnl = new JPanel();
-		pnl.setBackground(new Color(20,20,20));
+		pnl.setBackground(new Color(20,150,20));
 		JButton btn = new JButton("첫번째버튼");
+		JButton btn2 = new JButton("두번째버튼");
+		
 		
 		add(pnl);
 		pnl.add(btn);
+		pnl.add(btn2);
 		setSize(500, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
