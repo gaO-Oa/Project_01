520ba40436d8ee028d487e0d48e1c5b7992d126b
GGG <GGG@DESKTOP-8N3GG4O>
2022-12-07 16:31:54 +0900

    이은진

--- 221207-01/src/gayeong.java
+++ 221207-01/src/gayeong.java
@@ -3,6 +3,7 @@ public class gayeong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("이은진");
 		System.out.println("가영조장");
 		System.out.println("가영안녕");
 		System.out.println("머고머고");
